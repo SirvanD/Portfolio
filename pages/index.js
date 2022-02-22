2cfa04b540fd3ffad9454921e7957820a0170b56
SirvanD <sirvansh@gmail.com>
2022-02-23 00:22:35 +1100

    added google analytics

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,14 @@ export default function Home() {
           content="Hi!, I am Sirvan Doukchi, a Full-Stack developer based in Sydney,Australia and this is my Portfolio, check out my projects to see what I've been up to."
         />
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FZT1RM9HE8"></script>
+         <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-FZT1RM9HE8');
+          </script>
       </Head>
       <NavBar />
       <About />
