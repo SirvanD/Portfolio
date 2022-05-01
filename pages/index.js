80c9f3e2779e10ee7b315bb78d82eb47f0f23227
SirvanD <sirvansh@gmail.com>
2022-05-01 18:27:25 +0430

    updated comment

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import NavBar from "../components/NavBar/NavBar";
 import Skills from "../components/Skills/Skills";
 //adding header title and favicon and footer
 //extra comment
-//extra comment 4
+//extra comment 5
 export default function Home() {
   return (
     <div>
