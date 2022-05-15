a0b446d51ce3a36f47379f9b1a5d5ba518a5ec3e
SirvanD <sirvansh@gmail.com>
2022-05-16 09:01:51 +1000

    updated CV

--- public/social.js
+++ public/social.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { GithubIcon, GmailIcon, LinkedinIcon } from "./../public/icons";
-//exporting social object 7
+//exporting social object 8
 export const socialData = [
   {
     icon: <GithubIcon />,
